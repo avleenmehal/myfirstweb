f1e9287d30cd5369923125a5f23e4f1b5ad49212
avleenmehal <avleenmehal@gmail.com>
2018-01-29 22:50:51 +0530

    first commit

---
+++ Resources/js/script.js
@@ -0,0 +1,12 @@
+//$(document).ready(function() {
+//  
+//    $('.js-features').waypoint(function(direction){
+//        if(direction == "down") {
+//            $('nav').addClass('sticky'); }
+//        else  { $('nav').removeClass('sticky')
+//            }                    
+//    
+//    }, {
+//        offset: '90%'
+//    })
+//});
